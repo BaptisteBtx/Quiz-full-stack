f1e679144e540de38aaffd8a3d32e48cf1884c6d
Xolwinter <84453801+Xolwinter@users.noreply.github.com>
2022-12-29 18:36:05 +0100

    NewQuizPage

--- quiz-ui/src/views/NewQuizPage.vue
+++ quiz-ui/src/views/NewQuizPage.vue
@@ -1,6 +1,25 @@
 <template>
-  <h1>New Quiz</h1>
-  <router-link to="/">Retour</router-link>
+
+  <div class="container px-4">
+    <h1>Nouveau Quiz</h1>
+    <p> Pour débuter un nouveau quiz, veuillez remplir ces informations :</p>
+    <div class="d-flex justify-content-around align-items-center">
+      <input class="p-3 border bg-light" placeholder="Pseudonyme" type="text" />
+
+      <button type="button" class="btn btn-secondary ">Lancer le quiz</button>
+
+
+
+    </div>
+    <div class="p-2"></div>
+    <div class="d-flex justify-content-around align-items-center">
+      <router-link class="btn p-2 border bg-light" to="/">Retour</router-link>
+    </div>
+  </div>
+
+
+
+
 </template>
 
 <script>
