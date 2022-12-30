0b134a40116406a22414ca3fd3148fca21a02774
Xolwinter <84453801+Xolwinter@users.noreply.github.com>
2022-12-30 19:33:29 +0100

    Props et Emits

--- quiz-ui/src/components/QuestionDisplay.vue
+++ quiz-ui/src/components/QuestionDisplay.vue
@@ -7,14 +7,18 @@ const props = defineProps({
   question: Object
 })
 
+const emits = defineEmits(["answer-selected"])
 
-console.log(props.question)
+let currentQuestion = props.question.currentQuestion
+console.log(currentQuestion)
 // Export default : remplacé par script setup
 
 </script>
 
 <template>
   <div class="question">
+    <p>{{ currentQuestion.questionTitle }}</p>
+    <a @click="$emit('answer-selected', 2)">{{ currentQuestion.questionDescription }}</a>
     <!-- <img v-if="question.image" :src="question.image" /> -->
   </div>
 </template>
