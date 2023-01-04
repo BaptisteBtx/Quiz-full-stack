caf1b81bb765eb7b30ca907eda7651ed69cb16ba
BaptisteBtx <baptiste.bontoux@gmail.com>
2023-01-04 20:26:27 +0100

    header size

--- quiz-ui/src/App.vue
+++ quiz-ui/src/App.vue
@@ -39,7 +39,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 header {
   line-height: 1.5;
-  max-height: 100vh;
+  /* max-height: 100vh; */
   display: flex;
   justify-content: space-around;
   margin-bottom: 20px;
