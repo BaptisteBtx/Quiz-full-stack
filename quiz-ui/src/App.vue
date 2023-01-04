884b1796d173f481161af01fae0f6523baf59e72
BaptisteBtx <baptiste.bontoux@gmail.com>
2023-01-04 20:19:12 +0100

    header background

--- quiz-ui/src/App.vue
+++ quiz-ui/src/App.vue
@@ -8,14 +8,14 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <div id="app-container">
     <header class="bg-light">
-      <div>
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-        <h2>Quiz app</h2>
+      <div id="logo-container">
+        <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="150" height="150" />
+        <h2>Quiz écologie</h2>
       </div>
       
 
       <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light" fixed-top>
+        <nav class="navbar navbar-expand-lg navbar-light" fixed-top>
           <RouterLink to="/">Home</RouterLink>
           <RouterLink to="/start_new_quiz">Démarrer le quiz !</RouterLink>
           <RouterLink to="/about">A propos</RouterLink>
@@ -43,24 +43,31 @@ header {
   display: flex;
   justify-content: space-around;
   margin-bottom: 20px;
+  /* background-color: grey; */
+  background-image: url("assets/header_bg.png");
+  background-position: right;
+  background-clip: border-box;
+  background-repeat: no-repeat;
 }
 
-/* .logo {
-  display: block;
-  margin: 0 auto 2rem;
-} */
+.logo {
+  align-self: center;
+}
+
 
 nav {
   width: 100%;
-  height: 100%;
+  height: 100%; 
   font-size: 16px;
   text-align: center;
+  background-color: transparent;
   
 }
 
 nav a.router-link-exact-active {
   /* color: hsla(0, 0%, 10%, 0.2);; */
-  background-color: hsla(0, 0%, 60%, 0.2);
+  background-color: hsla(138, 63%, 24%, 0.2);
+  /* background-color: transparent; */
 }
 
 /* nav a.router-link-exact-active:hover {
@@ -70,6 +77,8 @@ nav a.router-link-exact-active {
 nav a {
   display: inline-block;
   text-decoration: none;
+  color: rgb(8, 98, 6);
+  background-color: rgb(241, 244, 243);;
   padding: 0 1rem;
   height: 50px;
   line-height: 45px;
@@ -77,7 +86,7 @@ nav a {
   /* width: 25%; */
   flex-grow: 1;
   /* vertical-align: middle; */
-  border: 1px solid black;
+  border: 1px solid rgb(8, 98, 6);
   /* border-radius: 10px; */
 }
 
